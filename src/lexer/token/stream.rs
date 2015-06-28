1f6d5ff7da7aed52baefee7e2ff64f6bdb7c6401
Colin Kiegel <kiegel@gmx.de>
2015-06-28 15:21:19 +0200

    no reference counting for raw template

--- src/lexer/token/stream.rs
+++ src/lexer/token/stream.rs
@@ -11,11 +11,17 @@
  * @author Colin Kiegel <kiegel@gmx.de>
  */
 
-use super::Token;
-use super::Type;
+/////////////
+// imports //
+/////////////
+
+use std::convert::Into;
+use super::{Token, Type};
 use template;
 
-use std::rc::Rc;
+/////////////
+// exports //
+/////////////
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -24,43 +30,62 @@ pub struct Item {
     position:   usize,
 }
 
+impl Into<Token> for Item {
+    fn into(self) -> Token {
+        self.token
+    }
+}
+
 #[allow(dead_code)]
 #[derive(Default)]
-pub struct Stream {
+pub struct Stream<'a> {
     items: Vec<Item>,
-    template: Option<Rc<template::Raw>>,
+    template: Option<&'a template::Raw>,
 }
 
 #[allow(dead_code)]
 #[allow(unused_variables)]
-impl Stream {
+impl<'a> Stream<'a> {
     /// Constructor
-    pub fn new(template: Rc<template::Raw>) -> Stream {
+    pub fn new(template: &'a template::Raw) -> Stream<'a> {
         Stream {
             items: Vec::new(),
             template: Some(template),
         }
     }
-    
+
     pub fn push(&mut self, token: Token, position: usize) {
         self.items.push(Item {
             token: token,
             position: position,
         });
     }
-    
+
     pub fn is_eof(&self) -> bool {
         match self.items.last()  {
             Some(x) => x.token.is_type(Type::Eof),
             None    => true,
         }
     }
+
+    pub fn len(&self) -> usize {
+        self.items.len()
+    }
 }
 
-impl ToString for Stream {
+impl<'a> ToString for Stream<'a> {
     /// Returns a string representation of the token stream.
     fn to_string(&self) -> String {
         let v: Vec<String> = self.items.iter().map(|i| i.token.to_string()).collect();
         v.connect("\n")
+    }
+}
+
+impl<'a> IntoIterator for Stream<'a> {
+    type Item = self::Item;
+    type IntoIter = <Vec<self::Item> as IntoIterator>::IntoIter;//ExactSizeIterator;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.items.into_iter()
     }
 }
--- src/template/raw/cursor.rs
+++ src/template/raw/cursor.rs
@@ -11,68 +11,91 @@
  * @author Colin Kiegel <kiegel@gmx.de>
  */
 
+/////////////
+// imports //
+/////////////
+
 use error;
 use lexer::SyntaxError;
 use lexer::SyntaxErrorCode;
-use std::rc::Rc;
+
+/////////////
+// exports //
+/////////////
 
 pub type Position = usize;
 pub type Line = usize;
 
 #[derive(Debug)]
-#[allow(dead_code)]
-pub struct Cursor {
+pub struct Cursor<'a> {
     pos: Position,   // 0,..
     end: Position,   // 0,..
     lineno: Line,    // 1,..
-    template: Option<Rc<super::Raw>>, // TODO switch to pointer
-}
-
-impl Default for Cursor {
-    fn default() -> Cursor {
-        Cursor {
-            pos: 0,
-            end: 0,
-            lineno: 1,
-            template: None,
-            /* TODO store a slice of the code directly? */
-        }
-    }
+    template: &'a super::Raw, // TODO switch to pointer or slice
 }
 
 #[allow(dead_code)]
-#[allow(unused_variables)]
-impl Cursor {
-    pub fn new(template: Rc<super::Raw>) -> Cursor {
+impl<'a> Cursor<'a> {
+    pub fn new(template: &'a super::Raw) -> Cursor<'a> {
         Cursor {
             end: template.code.chars().count(),
-            template: Some(template),
-            .. Default::default()
+            template: template,
+            pos: 0,
+            lineno: 1,
         }
     }
-    
+
     pub fn move_by(&mut self, text: &str) -> Result<(), SyntaxError> { // TODO return a new slice??
         self.pos += text.chars().count();
         self.lineno += text.lines().count();
-        
+
         if self.pos > self.end {
-            self.pos = self.end;
             return err!("out of range", SyntaxErrorCode::Unknown);
         }
-        
+
         Ok(())
     }
-    
-    pub fn move_to(&mut self, pos: usize) -> Result<(),SyntaxError> {
-        unimplemented!(); // TODO - might want to switch to this for better integrity
+
+    /// moves the cursor to a new position
+    ///
+    /// # Panics
+    /// Panics if `pos` is greater than the template's length.
+    pub fn move_to(&mut self, pos: usize) {
+        if pos > self.pos {
+            if pos > self.end {
+                panic!("Cursor::move_to() is out of range")
+            }
+            self.lineno += self.template.code[self.pos..pos].lines().count();
+        } else if pos < self.pos {
+            self.lineno -= self.template.code[pos..self.pos].lines().count();
+        }
+        self.pos = pos;
+    }
+
+    pub fn move_to_end(&mut self) {
+        self.lineno += self.template.code[self.pos..self.end].lines().count();
+        self.pos = self.end;
+    }
+
+    pub fn slice_to_end(&mut self) -> &str {
+        let ref slice = &self.template.code[self.pos..];
+
+        self.lineno += slice.lines().count();
+        self.pos = self.end;
+
+        slice
     }
 
     pub fn is_eof(&self) -> bool {
         self.pos >= self.end
     }
-    
-    pub fn get_position(&self) -> Position {
+
+    pub fn get_position(&self) -> usize {
         self.pos
+    }
+
+    pub fn get_line(&self) -> usize {
+        self.lineno
     }
 }
 
@@ -80,21 +103,20 @@ impl Cursor {
 mod test {
     use super::*;
     use super::super::Raw;
-    use std::rc::Rc;
 
     #[test]
     pub fn new() {
-        let tpl = Rc::new(Raw::new("123", ""));
-        let t_o = Cursor::new(tpl);
-        let t_x = Cursor {
+        let tpl = Raw::new("123", "");
+        let c_o = Cursor::new(&tpl);
+        let c_x = Cursor {
             pos: 0,
             end: 3,
             lineno: 1,
-            template: None, // not tested
+            template: &tpl,
         };
-        
-        assert_eq!(t_o.pos, t_x.pos);
-        assert_eq!(t_o.end, t_x.end);
-        assert_eq!(t_o.lineno, t_x.lineno);
+
+        assert_eq!(c_o.pos, c_x.pos);
+        assert_eq!(c_o.end, c_x.end);
+        assert_eq!(c_o.lineno, c_x.lineno);
     }
 }
