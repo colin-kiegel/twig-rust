f9db4e0edd681cea9e3e3b6b3774c7ed007ee55c
Colin Kiegel <kiegel@gmx.de>
2015-11-01 18:38:39 +0100

    runtime::api::Execute - run()

--- src/runtime/api/mod.rs
+++ src/runtime/api/mod.rs
@@ -22,4 +22,20 @@ use std::fmt::Debug;
 
 pub trait Execute : Debug {
     fn execute(&self, runtime: &Runtime, job: &mut Job);
+
+    // NOTE: This function is *not* object safe, because
+    //      runtime.run() requires a cast of self to object type `Execute`
+    //      This limitation of rust seems awkward, because except for the
+    //      type erasure this function would be object safe.
+    //
+    //      It feels like type erasure should be 'idempotent', i.e.
+    //      type erasure on a trait object should just return the trait
+    //      object. Thus type erasure should *not* require Self:Sized
+    //
+    // TODO: report to upstream rust
+    fn run(&self, runtime: &Runtime) -> String where
+        Self: Sized
+    {
+        runtime.run(self)
+    }
 }
