6935301be15461ce6b7b9ff420d8d2e11f7a6af1
Colin Kiegel <kiegel@gmx.de>
2015-11-29 22:28:14 +0100

    doc example

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,28 @@
 // For the copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
-/// Twig library for rust.
+//! Twig library for rust.
+//!
+//! # Examples
+//!
+//! ```
+//! use twig::compiler;
+//! use twig::loader;
+//! use twig::runtime::Runtime;
+//! use twig::template::api::Template;
+//!
+//! let mut loader = loader::array::Array::default();
+//! loader.set_template("greetings","Hello {{name}}!");
+//!
+//! let mut compiler = compiler::Builder::default().compiler().unwrap();
+//! compiler.set_loader(Box::new(loader));
+//!
+//! let mut runtime = Runtime::default();
+//! runtime.set("name", "world");
+//!
+//! let compiled = compiler.load_template("greetings", None).unwrap();
+//! assert_eq!(&compiled.render(&runtime).unwrap(), "Hello world!")
+//! ```
 
 extern crate regex;
 
